564dfba4df457fd05fed0aff1fd835b45ac1d8c9
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-07-19 09:24:09 -0300

    create database

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { json } from "express";
+import "express-async-errors";
+import cors from "cors";
+import chalk from "chalk";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+const PORT = +process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(chalk.bold.redBright(`Server is up and runnig on port ${PORT}`));
+});
